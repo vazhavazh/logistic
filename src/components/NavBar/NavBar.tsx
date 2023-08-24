889139da9513b9f5d548cbee2f893569692c1e03
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-08-24 13:21:33 +0300

    Update NavBar.tsx

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
+import { FcAbout, FcDocument, FcGallery, FcContacts } from "react-icons/fc";
 
 type NavBarProps = {};
 
 const NavBar: React.FC<NavBarProps> = () => {
- const router = useRouter();
+	const router = useRouter();
 
- const isLinkActive = (href: string) => {
+	const isLinkActive = (href: string) => {
 		return router.pathname === href ? "active" : "";
- };
+	};
 
 	return (
 		<nav className='bg-yellow flex px-20 py-8 justify-between text-2xl text-main-gray'>
@@ -18,22 +19,37 @@ const NavBar: React.FC<NavBarProps> = () => {
 					<span>Головна</span>
 				</div>
 			</Link>
-			<Link href='/about'>
+			<Link
+				href='/about'
+				className='flex gap-1 items-center'>
+				<FcAbout
+					width={20}
+					height={20}
+				/>
 				<div className={`nav-link ${isLinkActive("/about")}`}>
 					<span>Про нас</span>
 				</div>
 			</Link>
-			<Link href='/documents'>
+			<Link
+				href='/documents'
+				className='flex gap-1 items-center'>
+				<FcDocument />
 				<div className={`nav-link ${isLinkActive("/documents")}`}>
 					<span>Документи</span>
 				</div>
 			</Link>
-			<Link href='/gallery'>
+			<Link
+				href='/gallery'
+				className='flex gap-1 items-center'>
+				<FcGallery />
 				<div className={`nav-link ${isLinkActive("/gallery")}`}>
 					<span>Галерея</span>
 				</div>
 			</Link>
-			<Link href='/contacts'>
+			<Link
+				href='/contacts'
+				className='flex gap-1 items-center'>
+				<FcContacts />
 				<div className={`nav-link ${isLinkActive("/contacts")}`}>
 					<span>Контакти</span>
 				</div>
